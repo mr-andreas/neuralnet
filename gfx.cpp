4376f6fe71326b13246270b88206631d28cbe636
Andreas Andersen <andreas@softwaredesign.se>
2013-06-23 22:11:57 +0200

    Skippable

--- gfx.cpp
+++ gfx.cpp
@@ -106,7 +106,7 @@ int sdlMainLoop(sdlgamestate_t *g) {
 
   SDL_Event event;
   bool gameRunning = true;
-  bool doPlot = true;
+  int plotPause = 0;
   unsigned int startTime, endTime;
   unsigned int frameCounter = 0;
 
@@ -119,7 +119,19 @@ int sdlMainLoop(sdlgamestate_t *g) {
       }
       
       if(event.type == SDL_KEYUP) {
-        doPlot = !doPlot;
+        if(event.key.keysym.sym == 'q') {
+          gameRunning = false;
+        }
+        
+        if(plotPause) {
+          plotPause = 0;
+        } else {
+          if(event.key.keysym.sym == 'n') {
+            plotPause = 1;
+          } else {
+            plotPause = 2;
+          }
+        }
       }
     }
 
@@ -128,10 +140,12 @@ int sdlMainLoop(sdlgamestate_t *g) {
     if(frameCounter++ % REPOPULATE_ON_FRAME == 0) {
       printf("Transplanting brains\n");
       brainTransplant(g->gamestate);
+      if(plotPause == 1)
+        plotPause = 0;
     }
     
     doTurn(g->gamestate);
-    if(doPlot) {
+    if(!plotPause) {
       plotMines(g);
       plotBullets(g);
       plotSweepers(g);
