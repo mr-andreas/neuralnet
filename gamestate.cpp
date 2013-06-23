0a9bd210e411955b6df1c462ee95c0c3c33057f5
Andreas Andersen <andreas@softwaredesign.se>
2013-06-23 17:02:04 +0200

    Work

--- gamestate.cpp
+++ gamestate.cpp
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include "SVector2D.h"
+
 Sweeper::Sweeper() {
   int posx = 0;
   int posy = 0;
@@ -14,17 +16,36 @@ Gamestate::Gamestate(int boardWidth, int boardHeight) {
   this->boardHeight = boardHeight;
 }
 
+SVector2D getClosestMine(Gamestate *gs, int x, int y) {
+  double closest_so_far = 99999;
+
+  SVector2D pos(x, y);
+  SVector2D vClosestObject(0, 0);
+
+  //cycle through mines to find closest
+  for (int i=0; i<gs->mines.size(); i++) {
+    SVector2D mine(gs->mines[i].posx, gs->mines[i].posy);
+    double len_to_object = Vec2DLength(mine - pos);
+
+    if (len_to_object < closest_so_far) {
+      closest_so_far  = len_to_object;
+      vClosestObject  = pos - mine;
+    }
+  }
+
+  return vClosestObject;
+}
+
 void moveSweeper(Gamestate *gs, Sweeper &sweeper) {
   vector<double> vals(4);
-  vals[0] = 0.0;
-  vals[1] = 0.0;
-  vals[3] = 0.0;
-  vals[4] = 0.0;
+  SVector2D closestMine = getClosestMine(gs, sweeper.posx, sweeper.posy);
+  vals[0] = closestMine.x;
+  vals[1] = closestMine.y;
+  vals[3] = cos(sweeper.rotation);
+  vals[4] = -sin(sweeper.rotation);
   vector<double> output = sweeper.brain.Update(vals);
   
   double lTrack, rTrack;
-//   lTrack = rand() / ((double)RAND_MAX/2) - 1.0;
-//   rTrack = rand() / ((double)RAND_MAX/2) - 1.0;
   
   lTrack = output[0];
   rTrack = output[1];
@@ -44,6 +65,11 @@ void moveSweeper(Gamestate *gs, Sweeper &sweeper) {
   //update position
   sweeper.posx += vLookAtX * speed;
   sweeper.posy += vLookAtY * speed;
+  
+  if(sweeper.posx > gs->boardWidth) sweeper.posx -= gs->boardWidth;
+  if(sweeper.posy > gs->boardHeight) sweeper.posy -= gs->boardHeight;
+  if(sweeper.posx < 0) sweeper.posx += gs->boardWidth;
+  if(sweeper.posy < 0) sweeper.posy += gs->boardHeight;
   
 //   printf(
 //     "Rotation: %f speed %f pos %d,%d lookat %f,%f degress %f\n", sweeper.rotation, speed, 
